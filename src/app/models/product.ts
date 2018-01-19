7e3289657baf1cf8cd2ac60f8697122202a14f95
Raineer <delaritaraineer81@gmail.com>
2018-01-19 19:41:45 +0800

    ngrx

---
+++ src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    product: string;
+    category: string;
+}
+
+
+export const MEAT_PRODUCTS: Product[] = [
+  {id: 1, title: 'Java Article 1', category: 'Frozen'},
+   {id: 2, title: 'Java Article 1', category: 'Frozen'}
+]
