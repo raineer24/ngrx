c70f04028d6758f3553d64d6d60e88979359a633
raineer <delaritarainer81@gmail.com>
2018-01-21 15:18:46 +0800

    Product actions added

--- src/app/actions/product.actions.ts
+++ src/app/actions/product.actions.ts
@@ -1,2 +1,26 @@
 import { Action } from '@ngrx/store';
 import { Product } from '../models/product';
+
+                            
+export const MEAT = 'Meat';
+
+export const SPECIALTY = 'Specialty'
+
+export const MY_PRODUCTS = 'FAVORITE_PRODUCTS';
+
+export class MeatProductsAction implements Action {
+    readonly type = MEAT;
+}
+
+export class SpecialtyProductsAction implements Action {
+    readonly type = SPECIALTY
+}
+
+
+export class FavoriteProductsAction implements Action {
+    readonly type = MY_PRODUCTS;
+
+    constructor(public payload: Product[]) {}
+}
+
+export type All = MeatProductsAction | SpecialtyProductsAction | FavoriteProductsAction;
