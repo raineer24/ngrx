75f40f9f416305db78c7441b5169e3e27e8d5a4b
raineer <delaritarainer81@gmail.com>
2018-01-21 17:04:18 +0800

    metareducer

---
+++ src/app/reducers/reducers.ts
@@ -0,0 +1,24 @@
+import { ActionReducerMap, ActionReducer, MetaReducer } from '@ngrx/store';
+
+import { AppState } from './app.states';
+
+import * as productReducer from './product.reducer';
+
+import { environment } from '../../environments/environment';
+
+
+export const reducers: ActionReducerMap<AppState> = {
+    productState: productReducer.reducer
+};
+
+export function loogger(reducer: ActionReducer<AppState>): ActionReducer<AppState> {
+    return function(state: AppState, action: any): AppState {
+            console.log('state', state);
+            console.log('state', action);
+            return reducer(state, action);
+    };
+}
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+    ? [logger]
+    : [];
